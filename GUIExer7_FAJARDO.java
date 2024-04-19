59ece864a25a2be570095f6a35752ff923d472c9
kenfaj <keennnfajardo45@gmail.com>
2024-04-19 19:05:48 +0800

    FINAL exer7

--- GUIExer7_FAJARDO.java
+++ GUIExer7_FAJARDO.java
@@ -2,8 +2,8 @@ import java.awt.*;
 public class GUIExer7_FAJARDO extends Frame{
     {
         this.setLayout(new BorderLayout());
-        this.setTitle("Silly String Game");
-        this.setPreferredSize(new Dimension(300, 100));
+        this.setTitle("MegaCalc");
+        this.setPreferredSize(new Dimension(400,300));
     }
 
     /**
@@ -14,10 +14,10 @@ public class GUIExer7_FAJARDO extends Frame{
 
         Panel south = new Panel(new GridLayout(1,1));
         north.add(new TextField("",5));
-        north.add(new Button("+"))
+        north.add(new Button("+"));
         north.add(new TextField("",5));
 
-
+        this.add(north, BorderLayout.NORTH);
 
 
         
