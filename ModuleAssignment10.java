0c9369f2e48844e1cfae3e3dd6bce3f9fbe6e9ae
kenfaj <149665960+kenfaj@users.noreply.github.com>
2024-04-23 19:26:17 +0800

    added java source mod10

---
+++ ModuleAssignment10.java
@@ -0,0 +1,8 @@
+import java.awt.Frame;
+
+public class ModuleAssignment10 extends Frame{
+    private Label lred,lgreen,lblue,lalpha;
+    private TextField tred,tgreen,tblue,talpha;
+    private Button bcompute,bclear;
+    private Panel 
+}
